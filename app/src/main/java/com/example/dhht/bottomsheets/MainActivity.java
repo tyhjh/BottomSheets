9f27ae4ae23395acec0afe57ca15e65ca0dbe515
HanPei <tyhj5@qq.com>
2018-07-25 15:15:26 +0800

    第二次提交

--- app/src/main/java/com/example/dhht/bottomsheets/MainActivity.java
+++ app/src/main/java/com/example/dhht/bottomsheets/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.dhht.bottomsheets;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.example.bottomsheet.DialogUtil;
+import com.example.bottomsheet.MyDialog;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +27,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        btn_hello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-
-
+                ImageView imageView = new ImageView(MainActivity.this);
+                imageView.setImageResource(R.drawable.ic_launcher_background);
+                MyDialog dialog = new MyDialog(MainActivity.this, imageView, R.style.dialog2);
+                dialog.setCancelable(true);
+                dialog.show();
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            }
+        });
     }
 }
